345a4b0a4924363b60848a6fe6434ea540b80197
Iasmin Rabelo <iasmin_rabelo@estudante.sesisenai.org.br>
2025-08-25 09:53:10 -0300

    fim da 8

--- exercicio_8.php
+++ exercicio_8.php
@@ -1,41 +1,39 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 8 - Números Pares</title>
 </head>
+
 <body>
-
-
     <form method="POST" action="">
-        <label for="numero">Digite um número:</label>
+        <lable for="numero">Digite um número:</lable>
         <input type="number" id="numero" name="numero" required>
-        <button type="submit" name="divisores">Verificar</button>
+        <button type="submit" name="NP">Verificar</button>
     </form>
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['divisores'])) {
+        if (isset($_POST['NP'])) {
             $numero = $_POST['numero'];
 
-            $divisores = array();
-            for ($i = 1; $i <= $numero; $i++) {
-                if ($numero % $i == 0) {
-                    $divisores[] = $i;
-                }
+
+            if ($numero % 2 == 0) {
+                echo "O número $numero é <strong>par<strong><br/>";
+            } else {
+                echo "O número $numero é <strong>ímpar<strong><br/>";
             }
 
-            $tamanho = count(value: $divisores);
-
-            for ($i = 1; $i < $tamanho; $i++) {
-                echo "" . $divisores[$i] . "<br>";
-            }
+            $quantidade_pares = floor($numero / 2);
+            
+            echo "Existem " . $quantidade_pares . " números pares entre 1 e " . $numero . ".";
         }
     }
 
     ?>
 
-    
 </body>
+
 </html>
