b7d6d4664c3ab71511fbdbb4731d500fd2cf723a
Iasmin Rabelo <iasmin_rabelo@estudante.sesisenai.org.br>
2025-08-29 11:14:28 -0300

    change

--- exercicio_9.php
+++ exercicio_9.php
@@ -3,24 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 9 -  Dois números e a soma.</title>
+    <title>Exercício 9 -  Dois números e a soma</title>
 </head>
 <body>
 
  <form method="POST" action="">
-        <lable for="numero">Digite um número:</lable>
+        <label for="numero">Digite o primeiro número:</label>
         <input type="number" id="numero" name="numero" required>
-        <button type="submit" name="NP">Verificar</button>
+        <label for="numero2">Digite o segundo número:</label>
+        <input type="number" id="numero2" name="numero2" required>
+        <button type="submit" name="dns">Verificar</button>
     </form>
+    
 <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['NP'])) {
+        if (isset($_POST['dns'])) {
             $numero = $_POST['numero'];
             $numero2 = $_POST['numero2'];
-                
             $soma = $numero + $numero2;
+            echo "A soma entre $numero e $numero2 é igual a $soma.";
         }
-        echo "A soma entre $numero e $numero2 é igual a $soma.";
+        
     }
 ?>
 </body>
