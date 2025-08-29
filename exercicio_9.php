0d2735e3d570f5438513e2522fa089909a7c2f3b
Iasmin Rabelo <iasmin_rabelo@estudante.sesisenai.org.br>
2025-08-29 10:52:33 -0300

    atividade 9

--- exercicio_9.php
+++ exercicio_9.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 10 -  Dois números e a soma.</title>
+    <title>Exercício 9 -  Dois números e a soma.</title>
 </head>
 <body>
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['NP'])) {
+            $numero = $_POST['numero'];
+            $numero2 = $_POST['numero2'];
+                
+            $soma = $numero + $numero2;
     
+            echo "A soma entre $numero e $numero2 é igual a $soma.";
+        }
+    }
+?>
 </body>
 </html>
