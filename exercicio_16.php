b8d90e0810769af955e6944c603993c440c39815
iasmin-rabelo20 <iasmin_rabelo@estudante.sesisenai.org.br>
2025-09-02 07:30:55 -0300

    att 16

---
+++ exercicio_16.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 16 - Receber nome e idade e informar se a pessoa pode votar.</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <label for="numero">Digite o seu nome:</label>
+        <input type="text" name="nome" required><br><br>
+        <label for="numero">Digite a sua idade:</label>
+        <input type="number" name="numero" required><br><br>
+        <button type="submit" name="verificar">Enviar</button>
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (isset($_POST['verificar'])){
+                $nome = $_POST['nome'];
+                $numero = $_POST['numero'];
+
+                if ($numero >= 18){
+                    echo "$nome, você pode votar.";
+                }else{
+                    echo "$nome, você não pode votar.";
+                }
+            }
+        }
+    ?>
+
+    
+</body>
+</html>
